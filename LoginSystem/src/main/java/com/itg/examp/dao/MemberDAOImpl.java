c289e55b1883a89fad6deef67c6280405d151dfb
itg <itg@DESKTOP-CNVU1CQ>
2025-01-21 14:56:12 +0900

    update

---
+++ LoginSystem/src/main/java/com/itg/examp/dao/MemberDAOImpl.java
@@ -0,0 +1,47 @@
+package com.itg.examp.dao;
+
+
+import java.util.List;
+import java.util.Map;
+
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import com.itg.examp.dto.MemberDTO;
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+	@Autowired
+	SqlSessionTemplate session;
+	@Override
+	public int signupMember(MemberDTO member) {
+		System.out.println(member.getMid());
+		System.out.println(member.getMpw());
+		System.out.println(member.getName());
+		System.out.println(member.getAddress());
+		System.out.println(member.getAge());
+    	int res =  session.insert("memberMapper.signup",member);
+    	System.out.println("회원가입DAO:"+res);
+    	return res;
+	}
+	@Override
+	public MemberDTO signinMemeber(Map<String, String> data) {
+		MemberDTO member = session.selectOne("memberMapper.signin", data);
+    	return member;
+	}
+	@Override
+	public List<MemberDTO> memberList() {
+		List<MemberDTO> ll = session.selectList("memberMapper.memberList");
+    	return ll;
+	}
+	@Override
+	public MemberDTO signinMember(Map<String, String> data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+
+
