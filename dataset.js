ebbbf27ab55ea4d0d5d26ec7db20aa2abde8dffe
itg <itg@DESKTOP-CNVU1CQ>
2025-01-24 15:30:22 +0900

    update

--- dataset.js
+++ dataset.js
@@ -48,26 +48,33 @@ data_sets.push(d1)
 
 // menu2 =============================================================
 let d2 = new DataSet("공통모듈구현")//메인타이틀
+d2.set_content("공통모듈구현 작동페이지, 로그인화면, 소스코드 ")
+d2.set_img(0,{imgtitle:"공통모듈구현",imgurl:"https://drive.google.com/file/d/1HKmqdt88GHiAhLsceNsJ8fX4p-QKsYVo/view?usp=drive_link",imglog:"공통모듈구현으로 로그인UI 연동 및 로그아웃 UI연동 구현",sourceurl:"https://github.com/jihoonab/loginsystem/blob/main/LoginSystem/src/main/resources/static/common.js"})//서브 타이틀
+d2.set_img(0,{imgtitle:"공통모듈구현 작동 페이지",imgurl:"https://drive.google.com/file/d/1-A9zAVFdYbgGgcNQ5IhJjb5bu2bDBqkW/view?usp=drive_link",imglog:"공통모듈구현 쟉둉 페이지 결과",sourceurl:""})
+d2.set_img(0,{imgtitle:"공통모듈구현 로그인 화면",imgurl:"https://drive.google.com/file/d/1R9CbpvCjT-Z4O85doOLP-IF5P72kCLSE/view?usp=drive_link",imglog:"공통모듈구현으로 로그인 시 출력되는 결과",sourceurl:""})
+d2.set_img(0,{imgtitle:"공통모듈구현 소스코드",imgurl:"https://drive.google.com/file/d/1AqLqry307aDGnY0ZHfyVjI5_lY6LabJ6/view?usp=drive_link",imglog:"공통모듈구현 소스코드",sourceurl:""})
 
-d2.set_content(0,{imgtitle:"공통모듈구현",imgurl:"https://drive.google.com/file/d/1HKmqdt88GHiAhLsceNsJ8fX4p-QKsYVo/view?usp=drive_link",imglog:"공통모듈구현으로 로그인UI 연동 및 로그아웃 UI연동 구현",sourceurl:"https://github.com/jihoonab/loginsystem/blob/main/LoginSystem/src/main/resources/static/common.js"})//서브 타이틀
-d2.set_content(0,{imgtitle:"공통모듈구현 작동 페이지",imgurl:"https://drive.google.com/file/d/1-A9zAVFdYbgGgcNQ5IhJjb5bu2bDBqkW/view?usp=drive_link",imglog:"공통모듈구현 쟉둉 페이지 결과",sourceurl:""})
-d2.set_content(0,{imgtitle:"공통모듈구현 로그인 화면",imgurl:"https://drive.google.com/file/d/1R9CbpvCjT-Z4O85doOLP-IF5P72kCLSE/view?usp=drive_link",imglog:"공통모듈구현으로 로그인 시 출력되는 결과",sourceurl:""})
-d2.set_content(0,{imgtitle:"공통모듈구현 소스코드",imgurl:"https://drive.google.com/file/d/1AqLqry307aDGnY0ZHfyVjI5_lY6LabJ6/view?usp=drive_link",imglog:"공통모듈구현 소스코드",sourceurl:""})
+d2.set_img(1,{imgtitle:"테스트케이스",imgurl:"https://drive.google.com/file/d/1s1Qt07JyP-tytNNwhA-qPHLn7y40fm4X/view?usp=drive_link",imglog:"테스트 단계, 경계 입력값, 예상 출력값, 성공/실패 여부",sourceurl:"https://docs.google.com/document/d/1z-mgVYYfSK3pjqz4jIY1Ihz3K1e_mtxYplEnszlPq6s/edit?usp=drive_link"})//서브 타이틀
 
-d2.set_content(1,{imgtitle:"테스트케이스",imgurl:"https://drive.google.com/file/d/1s1Qt07JyP-tytNNwhA-qPHLn7y40fm4X/view?usp=drive_link",imglog:"테스트 단계, 경계 입력값, 예상 출력값, 성공/실패 여부",sourceurl:"https://docs.google.com/document/d/1z-mgVYYfSK3pjqz4jIY1Ihz3K1e_mtxYplEnszlPq6s/edit?usp=drive_link"})//서브 타이틀
-
-d2.set_content(2,{imgtitle:"단위테스트",imgurl:"https://docs.google.com/document/d/1xjW2afSCnPfZbgLHJdqgygicakKHQtHv3eg5c7X8BUc/edit?usp=drive_link",imglog:"단위테스트 로그인,로그아웃 기능 구현 테스트 성공여부",sourceurl:"https://docs.google.com/document/d/1z-mgVYYfSK3pjqz4jIY1Ihz3K1e_mtxYplEnszlPq6s/edit?usp=drive_link"})//서브 타이틀
-d2.set_content(2,{imgtitle:"단위테스트 로그인기능 구현",imgurl:"https://drive.google.com/file/d/1cswKof3EizBHKI99fxG9OQIHAZXYH-ky/view?usp=drive_link",imglog:"단위테스트 로그인 구현",sourceurl:""})
-d2.set_content(2,{imgtitle:"단위테스트 로그아웃 기능 구현",imgurl:"https://drive.google.com/file/d/1lJzq6DRWE9PGL_QDRERcRFE1n_XLLSkU/view?usp=drive_link",imglog:"단위테스트 로그아웃 구현",sourceurl:""})
+d2.set_img(2,{imgtitle:"단위테스트",imgurl:"https://docs.google.com/document/d/1xjW2afSCnPfZbgLHJdqgygicakKHQtHv3eg5c7X8BUc/edit?usp=drive_link",imglog:"단위테스트 로그인,로그아웃 기능 구현 테스트 성공여부",sourceurl:"https://docs.google.com/document/d/1z-mgVYYfSK3pjqz4jIY1Ihz3K1e_mtxYplEnszlPq6s/edit?usp=drive_link"})//서브 타이틀
+d2.set_img(2,{imgtitle:"단위테스트 로그인기능 구현",imgurl:"https://drive.google.com/file/d/1cswKof3EizBHKI99fxG9OQIHAZXYH-ky/view?usp=drive_link",imglog:"단위테스트 로그인 구현",sourceurl:""})
+d2.set_img(2,{imgtitle:"단위테스트 로그아웃 기능 구현",imgurl:"https://drive.google.com/file/d/1lJzq6DRWE9PGL_QDRERcRFE1n_XLLSkU/view?usp=drive_link",imglog:"단위테스트 로그아웃 구현",sourceurl:""})
 d2.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d2)
 
 // menu3 =============================================================
-let d3 = new DataSet("서버프로그램구현")//메인타이틀
-d3.set_content("회원가입구현")//서브타이틀
-d3.set_content("회원로그인구현")
-d3.set_content("회원리스트연동")
-d3.set_content("회원리스트출력구현")
+let d3 = new DataSet("회원가입구현")//메인타이틀
+d2.set_content("회원가입구현, 회원로그인,회원리스트, 리스트 출력결과")
+d3.set_img(0,{imgtitle:"회원가입화면",imgurl:"https://drive.google.com/file/d/1IcEFg6UCqQFSnmdmbp9ZB8RgbaQUu4M8/view?usp=drive_link",imglog:"회원가입을 하기 위한 회원정보입력",sourceurl:"https://github.com/jihoonab/loginsystem/blob/main/LoginSystem/src/main/resources/static/signup.html"})//서브타이틀
+d3.set_img(0,{imgtitle:"회원가입코드",imgurl:"https://drive.google.com/file/d/1ae0H4xOrLhkjtsUHZDR0iVS9d05yJaKK/view?usp=drive_link",imglog:"회원가입 코드",sourceurl:"https://docs.google.com/document/d/1I2QVcbcgC0IsZF6x76i5fi2SuyCDQideGVI7aak2uz0/edit?usp=drive_link"})
+d3.set_img(0,{imgtitle:"회원가입 제약조건",imgurl:"https://drive.google.com/file/d/10n3rFkJxwrXO5ly0-hEOwXA_hhU2Mqrl/view?usp=drive_link",imglog:"회원가입을 위한 제약조건",sourceurl:"https://docs.google.com/document/d/1I2QVcbcgC0IsZF6x76i5fi2SuyCDQideGVI7aak2uz0/edit?usp=drive_link"})
+d3.set_img(0,{imgtitle:"회원가입성공 화면",imgurl:"https://drive.google.com/file/d/1lWUg0VjMkS5CNqxHQEBtoa8cTNuzJl9-/view?usp=drive_link",imglog:"회원가입 성공화면",sourceurl:""})
+
+d3.set_img("회원로그인구현")
+
+d3.set_img("회원리스트연동")
+
+d3.set_img("회원리스트출력구현")
 d3.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 data_sets.push(d3)
 
